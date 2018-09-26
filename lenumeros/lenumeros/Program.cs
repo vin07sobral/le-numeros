351441e2b180cf5a73e7ff6952856b4ae400911c
Marcus Vinicius Sobral <mvinicius.sobral@gmail.com>
2018-09-26 16:31:32 -0300

    Add files via upload

---
+++ lenumeros/lenumeros/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace revisao_logica_de_programacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, quadrado;
+            x = int.Parse(Console.ReadLine());
+            while (x != 0)
+            {
+                quadrado = x * x;
+                Console.WriteLine(quadrado);
+                x = int.Parse(Console.ReadLine());
+            }
+            Console.ReadLine();
+        }
+    }
+}
